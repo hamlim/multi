79c26e1f6ce5adda30bc2f2368a374744298df7d
Matt Hamlin <matthewjameshamlin@gmail.com>
2025-05-28 21:25:05 -0400

    More messing around

--- commands/bootstrap.ts
+++ commands/bootstrap.ts
@@ -3,8 +3,12 @@ import { Console } from "effect";
 
 export let bootstrapCommand = Command.make(
   "bootstrap",
-  { name: Args.text({ name: "name" }) },
+  {
+    name: Args.text({ name: "name" }).pipe(
+      Args.withDescription("The name of the multi project"),
+    ),
+  },
   ({ name }) => {
     return Console.log(`Bootstrapping new multi-managed project${name}!`);
   },
-);
+).pipe(Command.withDescription("Initialize a new multi project."));
--- commands/init.ts
+++ commands/init.ts
@@ -3,8 +3,16 @@ import { Console } from "effect";
 
 export let initCommand = Command.make(
   "init",
-  { name: Args.text({ name: "name" }) },
+  {
+    name: Args.text({ name: "name" }).pipe(
+      Args.withDescription("The name of the workspace within the project"),
+    ),
+  },
   ({ name }) => {
     return Console.log(`Initializing ${name}!`);
   },
+).pipe(
+  Command.withDescription(
+    "Initialize a new workspace as part of a multi project.",
+  ),
 );
