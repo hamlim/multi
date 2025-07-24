2f75f5cbd9975cf6ad5b7f9b651488c59882ed68
Matt Hamlin <matthewjameshamlin@gmail.com>
2025-07-24 11:17:15 -0400

    Move to src dir

--- index.ts
+++ index.ts
@@ -3,8 +3,8 @@ import { NodeContext, NodeRuntime } from "@effect/platform-node";
 import { Console, Effect } from "effect";
 import { version } from "./package.json" with { type: "json" };
 
-import { bootstrapCommand } from "./commands/bootstrap";
-import { initCommand } from "./commands/init";
+import { bootstrapCommand } from "./src/commands/bootstrap";
+import { initCommand } from "./src/commands/init";
 
 let cliApp = Command.make("multi", {}, () => {
   return Console.log("Welcome to the multi CLI");
