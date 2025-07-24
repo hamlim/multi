91a045a2caa9dbfa366b5c447162ec97fec1f1b3
Matt Hamlin <matthewjameshamlin@gmail.com>
2025-07-24 11:20:25 -0400

    Start to scaffold out some shared types - defining things

---
+++ src/types.ts
@@ -0,0 +1,13 @@
+export type Package = {
+  /**
+   * Path to the package on the local file system
+   */
+  path: string;
+};
+
+export type Repository = {
+  /**
+   * Path to the repository on the local file system
+   */
+  path: string;
+};
