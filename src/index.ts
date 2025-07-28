ba1ebf83f5e67296d6ef40b2585cda974213e066
Matt Hamlin <matthewjameshamlin@gmail.com>
2025-07-28 06:53:10 -0400

    Fix imports

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,9 @@
 import { Command } from "@effect/cli";
 import { NodeContext, NodeRuntime } from "@effect/platform-node";
 import { Console, Effect } from "effect";
-import { version } from "./package.json" with { type: "json" };
-
-import { bootstrapCommand } from "./src/commands/bootstrap";
-import { initCommand } from "./src/commands/init";
+import { version } from "../package.json" with { type: "json" };
+import { bootstrapCommand } from "./commands/bootstrap";
+import { initCommand } from "./commands/init";
 
 let cliApp = Command.make("multi", {}, () => {
   return Console.log("Welcome to the multi CLI");
